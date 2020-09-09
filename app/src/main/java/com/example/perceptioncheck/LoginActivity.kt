e0147042c7284516b88fbde47b42a060d34ccaf9
Patrick Sorensen <pds4539@rit.edu>
2020-09-09 19:20:11 -0400

    Finished activity main and login and ready for firebase implementation

--- app/src/main/java/com/example/perceptioncheck/LoginActivity.kt
+++ app/src/main/java/com/example/perceptioncheck/LoginActivity.kt
@@ -1,11 +1,33 @@
 package com.example.perceptioncheck
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        login_button_login.setOnClickListener {
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+
+            login_progressbar_login.visibility = View.VISIBLE
+
+            Log.d("LoginActivity", "Email is: " + email)
+            Log.d("LoginActivity", "Password: $password")
+        }
+
+        back_to_register_textview_login.setOnClickListener {
+            Log.d("LoginActivity", "Try to show main activity")
+
+            // launch main activity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/perceptioncheck/MainActivity.kt
+++ app/src/main/java/com/example/perceptioncheck/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.example.perceptioncheck
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        register_button_register.setOnClickListener {
+            performRegister()
+        }
+
+        already_have_account_textview_register.setOnClickListener {
+            Log.d("MainActivity", "Try to show login activity")
+
+            // Launch login activity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun performRegister() {
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter text in email/password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if(password.length < 6) {
+            Toast.makeText(this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        register_progressbar_register.visibility = View.VISIBLE
+
+        Log.d("MainActivity", "Email is: " + email)
+        Log.d("MainActivity", "Password: $password")
+
+        // Firebase Authentication to create a user with email and password
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {
+                if(it.isSuccessful) {
+                    Log.d("MainActivity", "Successfully created user")
+                } else {
+                    Log.d("MainActivity", "Failed to create a user")
+                    return@addOnCompleteListener
+                }
+            }
+            .addOnFailureListener{
+                Log.d("MainActivity", "Failed to create user: ${it.message}")
+            }
     }
 }
